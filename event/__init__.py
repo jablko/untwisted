8c9e6aca8e3cd1bed6bbd59131f68e616cf78502
Jack Bates <ms419@freezone.co.uk>
2011-05-04 09:40:04 -0700

    Cosmetic

--- event/__init__.py
+++ event/__init__.py
@@ -11,6 +11,15 @@ import functools, sys, traceback, untwisted
 # twice.  By default .throw() raises the exception?  It calls .throw() on
 # callbacks passed to .connect()?
 class event:
+  def connect(ctx, callback):
+    ctx.callback.append(callback)
+
+    # Ready to propagate
+    if hasattr(ctx, 'advance'):
+      ctx.propagate()
+
+    return ctx
+
   def propagate(ctx):
     while True:
       try:
@@ -21,34 +30,34 @@ class event:
         return ctx
 
       if isinstance(callback, event):
-        callback.next = ctx.next
+        callback.advance = ctx.advance
         callback.propagate()
 
-        ctx.next = lambda callback: callback()
+        ctx.advance = lambda callback: callback()
 
         continue
 
       try:
-        result = ctx.next(callback)
+        result = ctx.advance(callback)
 
       except:
         final = untwisted.final(functools.partial(sys.stderr.write, ''.join(traceback.format_stack(sys._getframe().f_back)) + traceback.format_exc()))
 
         info = sys.exc_info()
-        ctx.next = lambda callback: (final.cancel(), callback.throw(*info))[-1]
+        ctx.advance = lambda callback: (final.cancel(), callback.throw(*info))[-1]
 
         continue
 
       if isinstance(result, event):
 
         # Don't propagate (on ctx.connect()) until callback
-        del ctx.next
+        del ctx.advance
 
         result.connect(ctx)
 
         return ctx
 
-      ctx.next = lambda callback: callback(result)
+      ctx.advance = lambda callback: callback(result)
 
   def __call__(ctx, *args, **kwds):
 
@@ -68,17 +77,8 @@ class event:
     except ValueError:
       pass
 
-    ctx.next = lambda callback: callback(*args, **kwds)
+    ctx.advance = lambda callback: callback(*args, **kwds)
     ctx.propagate()
-
-    return ctx
-
-  def connect(ctx, callback):
-    ctx.callback.append(callback)
-
-    # Ready to propagate
-    if hasattr(ctx, 'next'):
-      ctx.propagate()
 
     return ctx
 
@@ -108,7 +108,7 @@ class event:
 
     final = untwisted.final(functools.partial(sys.stderr.write, ''.join(traceback.format_stack(sys._getframe().f_back)) + traceback.format_exc()))
 
-    ctx.next = lambda callback: (final.cancel(), callback.throw(*args, **kwds))[-1]
+    ctx.advance = lambda callback: (final.cancel(), callback.throw(*args, **kwds))[-1]
     ctx.propagate()
 
     return ctx
