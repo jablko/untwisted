ac5bacbcea111db3a7e3dc3811b092615b256c60
Jack Bates <jack@nottheoilrig.com>
2011-09-16 11:35:31 -0700

    Parse ALTERNATE-SERVER attribute

--- stun.py
+++ stun.py
@@ -108,7 +108,7 @@ def request(server, messageMethod=binding):
     # Each STUN attribute MUST end on a 32-bit boundary
     recv = recv[4 + length - length % -4:]
 
-    if MAPPED_ADDRESS == type:
+    if type in (MAPPED_ADDRESS, ALTERNATE_SERVER):
 
       #  0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F
       # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
